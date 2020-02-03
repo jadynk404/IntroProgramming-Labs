bd7841d2fb1847d059bf8dfa2453aa92f0082b4f
Jadyn <jadyn.kennedy1@marist.edu>
2020-02-03 17:41:35 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,8 @@
+noun= input("enter a noun")
+verb= input("enter a verb")
+adjective= input("enter an adjective")
+place= input("enter a place")
+
+print("Once upon a time, there was a small ",noun," who loved to ",verb," and lived in a very ",adjective, place,". The end.")
+
+
