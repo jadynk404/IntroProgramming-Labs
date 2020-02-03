56d740675c3044be1abd60c341eeb09b2dbbc0e0
Jadyn <jadyn.kennedy1@marist.edu>
2020-02-03 17:12:34 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,3 @@
+distance=eval(input("How long does it take a photo from Curiosity to reach NASA when Mars is _____ miles away?"))
+speed=(distance/186000)
+print("It will take",speed, "seconds for a photo from Curiosity to reach Mars.")
