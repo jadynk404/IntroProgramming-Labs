764254eef4de3c506e8d753174692c4d7f8cdab9
Jadyn Kennedy <jadyn.kennedy1@marist.edu>
2020-02-06 18:31:17 -0500

    DRY code

--- game1.py
+++ game1.py
@@ -3,6 +3,7 @@
 # Citations: Used the template provided on iLearn
 
 def main():
+    e = "Press enter to continue..."
     
     print('Help the STEM major find her motivation!')
 
@@ -10,12 +11,12 @@ def main():
     print()
     print("Welcome to orienation! You begin your freshman year, ready and eager to learn!")
     print("GPA:", 4.0)
-    input("Press enter to continue...")
+    input(e)
     print("... Two weeks later ...")
     
     
     print("Oh no! You can't seem to find your motivation to continue! Where should you look?")
-    input("Press Enter to continue...")
+    input(e)
     print("You look through all the drawers in your room, making a mess in the process. There's nothing here.",
           "You know you have a",
           "project due in your Programming class in an hour, but you decide to clean up your mess instead.")
@@ -23,19 +24,19 @@ def main():
    
     print("You were late to class... with an incomplete project.")
     print("GPA:", 3.5)
-    input("Press Enter to continue...")
+    input(e)
 
     print("Congratulations! After your teacher scolded you for your project, you",
           "found a pinch of motivation on the floor and finally finish that project!",
           "You turn it in 2 days late and luckily your teacher takes pity",
           "on you and accepts it.")
     print("GPA:", 3.8)
-    input("Press enter to continue...")
+    input(e)
 
     print("You feel relieved to see your grades back up and decide to give yourself a nice break as a reward",
           "so you decide to skip your classes for the next day.")
     print("GPA:", 3.0)
-    input("Press enter to continue...")
+    input(e)
 
 
 
