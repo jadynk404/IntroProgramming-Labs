a1c3521860466f4315a83339c5e7c317de0d9eeb
Jadyn Kennedy <jadyn.kennedy1@marist.edu>
2020-02-06 17:57:58 -0500

    wrote basic script

---
+++ game1.py
@@ -0,0 +1,53 @@
+# Name: Jadyn Kennedy
+# Date: 2/5/2020
+# Citations: Used the template provided on iLearn
+
+def main():
+    
+    print('Help the STEM major find her motivation!')
+
+               
+    print()
+    print("Welcome to orienation! You begin your freshman year, ready and eager to learn!")
+    print("GPA:", 4.0)
+    input("Press enter to continue...")
+    print("... Two weeks later ...")
+    
+    
+    print("Oh no! You can't seem to find your motivation to continue! Where should you look?")
+    input("Press Enter to continue...")
+    print("You look through all the drawers in your room, making a mess in the process. There's nothing here.",
+          "You know you have a",
+          "project due in your Programming class in an hour, but you decide to clean up your mess instead.")
+
+   
+    print("You were late to class... with an incomplete project.")
+    print("GPA:", 3.5)
+    input("Press Enter to continue...")
+
+    print("Congratulations! After your teacher scolded you for your project, you",
+          "found a pinch of motivation on the floor and finally finish that project!",
+          "You turn it in 2 days late and luckily your teacher takes pity",
+          "on you and accepts it.")
+    print("GPA:", 3.8)
+    input("Press enter to continue...")
+
+    print("You feel relieved to see your grades back up and decide to give yourself a nice break as a reward",
+          "so you decide to skip your classes for the next day.")
+    print("GPA:", 3.0)
+    input("Press enter to continue...")
+
+
+
+    print("You start seeing assignments pile up in your peripheral vision as you watch netflix. You know you",
+          "should start them, but your motivation has once again run out.")
+    input("Press enter to look for some more!")
+    print("As you look start searching, you come across an email reminding you to keep up your grades so you",
+          "don't lose your full ride scholarship. Congratulations! That should be enough motivation to last you",
+          "untill you graduate! You power through all of your assignments and finish everything on time.")
+    print("GPA:", 4.0)
+
+    print("You have won! Please play again!")
+
+
+main()
