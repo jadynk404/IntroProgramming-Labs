590ac5803c4cfa3350c296c66a188ce3e82f5ffd
Jadyn Kennedy <jadyn.kennedy1@marist.edu>
2020-02-06 18:33:21 -0500

    DRYed out code more

--- game1.py
+++ game1.py
@@ -4,13 +4,14 @@
 
 def main():
     e = "Press enter to continue..."
+    g = "GPA:"
     
     print('Help the STEM major find her motivation!')
 
                
     print()
     print("Welcome to orienation! You begin your freshman year, ready and eager to learn!")
-    print("GPA:", 4.0)
+    print(g, 4.0)
     input(e)
     print("... Two weeks later ...")
     
@@ -23,19 +24,19 @@ def main():
 
    
     print("You were late to class... with an incomplete project.")
-    print("GPA:", 3.5)
+    print(g, 3.5)
     input(e)
 
     print("Congratulations! After your teacher scolded you for your project, you",
           "found a pinch of motivation on the floor and finally finish that project!",
           "You turn it in 2 days late and luckily your teacher takes pity",
           "on you and accepts it.")
-    print("GPA:", 3.8)
+    print(g, 3.8)
     input(e)
 
     print("You feel relieved to see your grades back up and decide to give yourself a nice break as a reward",
           "so you decide to skip your classes for the next day.")
-    print("GPA:", 3.0)
+    print(g, 3.0)
     input(e)
 
 
@@ -46,7 +47,7 @@ def main():
     print("As you look start searching, you come across an email reminding you to keep up your grades so you",
           "don't lose your full ride scholarship. Congratulations! That should be enough motivation to last you",
           "untill you graduate! You power through all of your assignments and finish everything on time.")
-    print("GPA:", 4.0)
+    print(g, 4.0)
 
     print("You have won! Please play again!")
 
