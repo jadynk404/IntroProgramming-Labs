837c2d74b482c3a5f8c2f3d2ddfd2d1ea2633996
Jadyn <jadyn.kennedy1@marist.edu>
2020-02-17 18:02:46 -0500

    usability improvements to guessing game

--- python_guessing_game.py
+++ python_guessing_game.py
@@ -5,11 +5,23 @@ def main():
     while x==1:
         y = input("I'm thinking of an animal. Can you guess what it is?\n")
         c = "python"
+        
         if c == y:
             x = 0
             print("Congratulations! You won!")
         else:
-            print("Sorry. That's not it. Try Again!\n")
+            p = input("Sorry. That's not it. Try Again?\n")
+            
+            if p[0] == "y":
+                x = 1    
+            else:
+                x = 0
+                print("Ok. Looks like someone's a sore loser. Seeya!")
+                
+                
+            
+
+        
 
 main()
         
